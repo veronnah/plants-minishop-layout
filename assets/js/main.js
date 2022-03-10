a9c0b456a2f87297caff5cb79da04903f020de0f
veronnah <yoo.veroon@gmail.com>
2022-03-10 22:28:28 +0100

    FAQ accordion added

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,15 +27,43 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById('header')
-    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
 
 
 /*=============== QUESTIONS ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.questions__item');
 
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.questions__header');
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open');
+
+        toggleItem(item);
+
+        if (openItem && openItem !== item) {
+            toggleItem(openItem);
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.questions__content');
+
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style');
+        item.classList.remove('accordion-open');
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px';
+        item.classList.add('accordion-open');
+    }
+
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
