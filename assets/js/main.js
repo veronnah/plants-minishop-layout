4130c0326dd40aa4bbcd351aaade72dcd0494a30
veronnah <yoo.veroon@gmail.com>
2022-03-11 14:20:15 +0100

    mobile version done

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,16 +62,33 @@ const toggleItem = (item) => {
         accordionContent.style.height = accordionContent.scrollHeight + 'px';
         item.classList.add('accordion-open');
     }
-
 }
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
 
 
 /*=============== SHOW SCROLL UP ===============*/
-
-
-/*=============== DARK LIGHT THEME ===============*/
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 400) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
